7c82d706ef0ec053992c4cb505500b28edacaead
Dipesh Paudel <thedeepyash7@gmail.com>
2024-09-02 08:27:49 +0545

    got 2 sol as airdrop

--- index.js
+++ index.js
@@ -15,13 +15,28 @@ const getWalletBalance = async () => {
   try {
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
     const walletBalance = await connection.getBalance(publicKey);
-    console.log(`Wallet balance is $${walletBalance}`);
+    console.log(`Wallet balance is ${walletBalance}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const airdropSOL = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const fromAirdropSignature = await connection.requestAirdrop(
+      publicKey,
+      2 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(fromAirdropSignature);
   } catch (error) {
     console.log(error);
   }
 };
 
 const main = async () => {
+  await getWalletBalance();
+  await airdropSOL();
   await getWalletBalance();
 };
 
